208a2f36f98354e0a2223b228df9d8f38ce6eb25
JagpreetRandio <115323708+JagpreetRandio@users.noreply.github.com>
2023-01-26 14:02:38 -0800

    finished schema and seeds code

---
+++ db/seeds.sql
@@ -0,0 +1,34 @@
+USE employeeTracker;
+
+INSERT INTO department (department_name)
+VALUES
+  ('HR'),
+  ('Tech'),
+  ('Marketing'),
+  ('Finance'),
+  ('Sales'),
+  ('Engineering'),
+  ('Legal'),
+  ('Executive');
+  
+INSERT INTO role (title, salary, department_id)
+VALUES
+  ('Recruiter ', 50000, 1),
+  ('Marketer', 65000, 3),
+  ('Software Engineer', 120000, 2),
+  ('Attorney', 200000, 7),
+  ('Salesperson', 130000, 5),
+  ('Engineer', 190000, 6),
+  ('Accountant', 160000, 7),
+  ('Sales Lead', 130000, 5),
+  ('CEO', 500000, 8);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+  ('Jagpreet', 'Randio', 8, 1),
+  ('Paris', 'Hilton', 2, 2),
+  ('London', 'Tipton', 3, 1),
+  ('Sharpay', 'Evans', 4, 3),
+  ('Elle', 'Woods', 5, 1),
+  ('Angelina', 'Jolie',1, 3),
+  ('Tanya', 'McQuid', 6, 3);
--- server.js
+++ server.js
@@ -3,13 +3,14 @@ const express = require('express');
 const inquirer = require("inquirer");
 const mysql = require("mysql");
 const console = require("console.table");
-const db = require('./db/connection');
+const connection = require('./db/connection');
 
 // setting Express app
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3002;
 const app = express();
 
 // Adding middleware 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
 
